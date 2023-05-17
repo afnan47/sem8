a01a61f0eb8557a91be317b240e0a6dcd90830fc
Afnan Attar <afnan.attar74@gmail.com>
2023-05-17 20:32:09 +0530

    Changes

--- HPC/2_parallel_bubble.cpp
+++ HPC/2_parallel_bubble.cpp
@@ -3,57 +3,50 @@
 using namespace std;
 
 
-void bubble(int a[], int n){
-    for(int i = 0; i < n; i++){            
-        if(a[i] > a[i + 1]) swap(a[i], a[i + 1]);
+void bubble(int array[], int n){
+    for (int i = 0; i < n - 1; i++){
+        for (int j = 0; j < n - i - 1; j++){
+            if (array[j] > array[j + 1]) swap(array[j], array[j + 1]);
+        }
     }
 }
 
-void pBubble(int a[], int n){
-    bool swapped;
-    for(int i = 0; i < n; i++){            
-        int first = i % 2;
-        swapped = false;
-        #pragma omp parallel for shared(a, first)
-        for(int j = first; j < n - 1;  j += 1){       
-            if(a[j] > a[j+1]){       
-                swap(a[j], a[j+1]);
-                swapped = true;
-             }       
-            }  
-        if(!swapped) break;
+void pBubble(int array[], int n){
+    int i,j;
+    #pragma omp parallel for shared(array, n) private(i, j)
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (array[j] > array[j + 1]) swap(array[j], array[j + 1]);
+        }
     }
 }
 
 int main(){
-    
-    // Difference in speeds for parralel and sequential will only come about when n is large.
-    int n = 100000;
+    int n = 10000;
     int a[n];
     double start_time, end_time;
 
-    // Create an array of n numbers, with digits from n to 1 in descending order
+    // Create an array of n numbers, with numbers from n to 1
     for(int i = 0, j = n; i < n; i++, j--) a[i] = j;
     
     // Create a copy 
     int b[n];
     for(int i = 0; i < n; i++) b[i] = a[i];
 
-
     // Measure Sequential Time
     start_time = omp_get_wtime(); 
     bubble(a,n);
     end_time = omp_get_wtime(); 
-    cout << "\nTime taken by sequential algorithm: " << end_time - start_time << " seconds\n";
+    cout << "Time taken by sequential algorithm: " << end_time - start_time << " seconds\n";
 
-     //Measure Parallel time
+    //Measure Parallel time
     start_time = omp_get_wtime(); 
     pBubble(b,n);
     end_time = omp_get_wtime(); 
     cout << "Time taken by parallel algorithm: " << end_time - start_time << " seconds";
-
-    // Unfortunately parallel algorithms only do well on large scales.
-    // In our case sequential may always do better than parallel.
-    // This is because parallel algos have the overhead of creating threads.
+    
+    /*
+    The parallel bubble sort algorithm does not do better than the sequential algorithm. In fact, it is actually slower in most cases. This is because the bubble sort algorithm is a very simple algorithm that does not lend itself well to parallelism. The overhead of synchronizing the threads and dividing the array into smaller parts outweighs the benefits of parallel execution.
+    */
     return 0;
 }
