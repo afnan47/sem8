27c0c91ed6404dcb108dad36d3797e968d2d3c3d
Afnan Attar <afnan.attar74@gmail.com>
2023-05-18 18:47:56 +0530

    Comments

--- HPC/3_parallel_reduction.cpp
+++ HPC/3_parallel_reduction.cpp
@@ -1,3 +1,9 @@
+/*
+  Windows does not support user defined reductions.
+  This program may not run on MVSC++ compilers for Windows.
+  Please use Linux Environment.[You can try using "windows subsystem for linux"]
+*/
+
 #include <iostream>
 #include <vector>
 #include <omp.h>
@@ -5,10 +11,10 @@
 
 using namespace std;
 
-void min_reduction(vector<int> arr){
+void min_reduction(int* arr, int n){
   int min_value = INT_MAX;
   #pragma omp parallel for reduction(min: min_value)
-  for (int i = 0; i < arr.size(); i++){
+  for (int i = 0; i < n; i++){
     if (arr[i] < min_value){
       min_value = arr[i];
     }
@@ -16,10 +22,10 @@ void min_reduction(vector<int> arr){
   cout << "Minimum value: " << min_value << endl;
 }
 
-void max_reduction(vector<int> arr){
+void max_reduction(int* arr, int n){
   int max_value = INT_MIN;
   #pragma omp parallel for reduction(max: max_value)
-  for (int i = 0; i < arr.size(); i++){
+  for (int i = 0; i < n; i++){
     if (arr[i] > max_value) {
       max_value = arr[i];
     }
@@ -27,40 +33,31 @@ void max_reduction(vector<int> arr){
   cout << "Maximum value: " << max_value << endl;
 }
 
-void sum_reduction(vector<int> arr){
+void sum_reduction(int* arr, int n){
   int sum = 0;
    #pragma omp parallel for reduction(+: sum)
-   for (int i = 0; i < arr.size(); i++){
+   for (int i = 0; i < n; i++){
     sum += arr[i];
   }
   cout << "Sum: " << sum << endl;
 }
 
-void average_reduction(vector<int> arr){
+void average_reduction(int* arr, int n){
   int sum = 0;
   #pragma omp parallel for reduction(+: sum)
-  for (int i = 0; i < arr.size(); i++){
+  for (int i = 0; i < n; i++){
     sum += arr[i];
   }
-  cout << "Average: " << (double)sum / arr.size() << endl;
+  cout << "Average: " << (double)sum / n << endl;
 }
 
 int main(){
-  vector<int> arr;
-  arr.push_back(5);
-  arr.push_back(2);
-  arr.push_back(9);
-  arr.push_back(1);
-  arr.push_back(7);
-  arr.push_back(6);
-  arr.push_back(8);
-  arr.push_back(3);
-  arr.push_back(4);
-
+  int arr[] = {5,2,9,1,7,6,8,3,4};
+  int n = 9;
   //Run ops
-  min_reduction(arr);
-  max_reduction(arr);
-  sum_reduction(arr);
-  average_reduction(arr);
+  min_reduction(arr, n);
+  max_reduction(arr, n);
+  sum_reduction(arr, n);
+  average_reduction(arr, n);
   return 0;
 }
