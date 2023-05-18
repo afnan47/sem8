463ee9de7040a94fb5fbc79466bc9ccc64bad4b4
Afnan Attar <afnan.attar74@gmail.com>
2023-05-18 17:16:40 +0530

    Better

--- HPC/2_parallel_merge.cpp
+++ HPC/2_parallel_merge.cpp
@@ -1,83 +1,104 @@
-#include<iostream>
-#include<omp.h>
+#include <iostream>
+#include <omp.h>
+
 using namespace std;
 
-void merge(int a[], int i1, int j1, int i2, int j2, int n){
-    int temp[n];    
-    int i, j, k;
-    i = i1;    
-    j = i2;    
-    k = 0;
-    while(i <= j1 && j <= j2){
-        if(a[i] < a[j]) temp[k++] = a[i++];
-        else temp[k++] = a[j++];
-	}
+void merge(int arr[], int low, int mid, int high) {
+    // Create arrays of left and right partititons
+    int n1 = mid - low + 1;
+    int n2 = high - mid;
+
+    int left[n1];
+    int right[n2];
+    for(int i = 0; i < n1; i++) left[i] = 0;
+    for(int i = 0; i < n2; i++) right[i] = 0;
+
+    // Copy all left elements
+    for (int i = 0; i < n1; i++) left[i] = arr[low + i];
     
-    while(i <= j1) temp[k++] = a[i++];
-    while(j <= j2) temp[k++] = a[j++];
-  
-    for(i = i1, j = 0; i <= j2; i++, j++) a[i] = temp[j];
-}
+    // Copy all right elements
+    for (int j = 0; j < n2; j++) right[j] = arr[mid + 1 + j];
+    
+    // Compare and place elements
+    int i = 0, j = 0, k = low;
 
-void mergesort(int a[],int i,int j, int n){
-    int mid;
-    if(i < j){
-        mid = (i + j) / 2;
-        mergesort(a, i, mid, n);        
-        mergesort(a, mid + 1, j, n);    
-        merge(a, i, mid, mid + 1, j, n);    
+    while (i < n1 && j < n2) {
+        if (left[i] <= right[j]){
+            arr[k] = left[i];
+            i++;
+        } 
+        else{
+            arr[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+
+    // If any elements are left out
+    while (i < n1) {
+        arr[k] = left[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = right[j];
+        j++;
+        k++;
     }
 }
 
-void pMergesort(int a[],int i,int j, int n){
-    int mid;
-    if(i < j){
-        mid = (i + j) / 2;
-        #pragma omp task firstprivate(a, i, j, n)
-        mergesort(a, i, mid, n);        
-        
-        #pragma omp task firstprivate(a, i, j, n)
-        mergesort(a, mid + 1, j, n);    
+void parallelMergeSort(int arr[], int low, int high) {
+    if (low < high) {
+        int mid = (low + high) / 2;
 
-        #pragma omp taskwait
-        merge(a, i, mid, mid + 1, j, n);    
+        #pragma omp parallel sections
+        {
+            #pragma omp section
+            {
+                parallelMergeSort(arr, low, mid);
+            }
+
+            #pragma omp section
+            {
+                parallelMergeSort(arr, mid + 1, high);
+            }
+        }
+        merge(arr, low, mid, high);
     }
 }
 
-int main()
-{
+void mergeSort(int arr[], int low, int high) {
+    if (low < high) {
+        int mid = (low + high) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+}
+
+int main() {
     int n = 10;
-    int a[n];
+    int arr[n];
     double start_time, end_time;
 
     // Create an array of n numbers, with digits from n to 1 in descending order
-    for(int i = 0, j = n; i < n; i++, j--) a[i] = j;
+    for(int i = 0, j = n; i < n; i++, j--) arr[i] = j;
     
-    // Create a copy 
-    int b[n];
-    for(int i = 0; i < n; i++) b[i] = a[i];
-
-
     // Measure Sequential Time
     start_time = omp_get_wtime(); 
-    mergesort(a, 0, n - 1, n);
+    mergeSort(arr, 0, n - 1);
     end_time = omp_get_wtime(); 
     cout << "Time taken by sequential algorithm: " << end_time - start_time << " seconds\n";
 
-     //Measure Parallel time
+    // Reset the array
+    for(int i = 0, j = n; i < n; i++, j--) arr[i] = j;
+    
+    //Measure Parallel time
     start_time = omp_get_wtime(); 
-    #pragma omp parallel
-    {
-        #pragma omp single
-        {
-            mergesort(b, 0, n - 1, n);
-        }
-    }
+    parallelMergeSort(arr, 0, n - 1);
     end_time = omp_get_wtime(); 
-    cout << "\nTime taken by parallel algorithm: " << end_time - start_time << " seconds";
-
-    // Unfortunately parallel algorithms only do well on large scales.
-    // In our case sequential may always do better than parallel.
-    // This is because parallel algorithms have the overhead of creating threads.
+    cout << "Time taken by parallel algorithm: " << end_time - start_time << " seconds";
+    
     return 0;
 }
