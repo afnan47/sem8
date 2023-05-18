6925539cf965652fecfd53b19cf00daf18972023
Afnan Attar <afnan.attar74@gmail.com>
2023-05-18 20:45:48 +0530

    Shorten code

--- HPC/4_Matrix_Multiplication.cpp
+++ HPC/4_Matrix_Multiplication.cpp
@@ -1,128 +1,94 @@
 %%cu
 #include <iostream>
-#include <math.h>
-#include <functional>
-#include <stdlib.h>    
-#include <time.h>       
+using namespace std;
 
-#define ROW_TILE_WIDTH 32
-#define COL_TILE_WIDTH 32
 
-template<typename T>
-__global__
-void naive_matrix_multiply(T *A, T *B, T* C, int width, int C_rows, int C_cols)
-{
-  int row = blockIdx.y * blockDim.y + threadIdx.y;   
-  int col = blockIdx.x * blockDim.x + threadIdx.x;
-  // check boundry conditions
-  if( row < C_rows && col < C_cols ){
-    // do the multiplication for one row and col
-    T value = 0;
-    for(int k = 0; k < width; k++){
-      value += A[row * width + k] * B[k * C_cols + col];
-    }
-    // store result
-    C[row * C_cols + col] = value;
-  }
-  
+// CUDA code to multiply matrices
+__global__ void multiply(int* A, int* B, int* C, int size) {
+    // Uses thread idices and block indices to compute each element
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
 
-}
-
-template<typename T>
-void initialize_matrix(T* M, int rows, int cols, std::function<float()> F) {
-  for(int i = 0; i < rows; i++){
-    for(int j = 0; j < cols; j++){
-      M[i * cols + j] = F();
-    }
-  }
-}
-
-template<typename T>
-void naive_matrix_multiply_cpu(T *A, T *B, T* C, int width, int C_rows, int C_cols){
-  for(int i = 0; i < C_rows; i++)
-    for(int j = 0; j < C_cols; j++){
-      T value = 0.0f;
-      for(int k = 0; k < width; k++){
-        value += A[i * width + k] * B[k * C_cols + j];
-      }
-      C[i * C_cols + j] = value;
+    if (row < size && col < size) {
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum += A[row * size + i] * B[i * size + col];
+        }
+        C[row * size + col] = sum;
     }
 }
 
-template<typename T>
-bool check_equal(T* A1, T* A2, int rows, int cols){
-  for(int i = 0; i < rows; i++)
-    for(int j = 0; j < cols; j++){
-      if(abs(A1[i * cols + j] - A2[i * cols + j]) > 0.00001){
-          return false;
-      }
+
+void initialize(int* matrix, int size) {
+    for (int i = 0; i < size * size; i++) {
+        matrix[i] = rand() % 10;
     }
-  
-  return true;
 }
 
 
-int main(void)
-{
-  int A_rows = 1 << 8;
-  int A_cols = 1 << 10;
-  int B_rows = A_cols;
-  int B_cols = 1 << 12;
-  int C_rows = A_rows;
-  int C_cols = B_cols;
-  int A_size = A_rows * A_cols;
-  int B_size = B_rows * B_cols;
-  int C_size = C_rows * C_cols;
-  float *A, *B, *C, *C_cpu;
-
-  // Allocate Unified Memory – accessible from CPU or GPU
-  cudaMallocManaged(&A, A_size*sizeof(float));
-  cudaMallocManaged(&B, B_size*sizeof(float));
-  cudaMallocManaged(&C, C_size*sizeof(float));
-  cudaMallocManaged(&C_cpu, C_size*sizeof(float));
-
-  // initialize A and B matrices
-  auto all_ones = []() -> float {
-    return 1.0f;
-  };
-
-  srand (time(NULL));
-  auto rand_numbers = []() -> float {
-    auto f = static_cast<float>(rand())/(static_cast<float>(RAND_MAX/1000));
-    int n = static_cast<int>(f);
-    return static_cast<float>(n);
-  };
-
-  initialize_matrix<float>(A, A_rows, A_cols, rand_numbers);
-  initialize_matrix<float>(B, B_rows, B_cols, rand_numbers);
-
-  dim3 dim_grid(C_cols/COL_TILE_WIDTH, C_rows/ROW_TILE_WIDTH, 1);
-  dim3 dim_block(COL_TILE_WIDTH, ROW_TILE_WIDTH, 1);
-
-  naive_matrix_multiply<float><<<dim_grid, dim_block>>>(A, B, C, A_cols, C_rows, C_cols);
-
-  // Wait for GPU to finish before accessing on host
-  cudaDeviceSynchronize();
-  
-  // check results
-  naive_matrix_multiply_cpu<float>(A, B, C_cpu, A_cols, C_rows, C_cols);
-  
-  
-  if(check_equal<float>(C, C_cpu, C_rows, C_cols))
-    std::cout << "PASS" << std::endl;
-  else
-    std::cout << "FAIL" << std::endl;
-
-  // Free memory
-  cudaFree(A);
-  cudaFree(B);
-  cudaFree(C);
-  
-  return 0; 
+void print(int* matrix, int size) {
+    for (int row = 0; row < size; row++) {
+        for (int col = 0; col < size; col++) {
+            cout << matrix[row * size + col] << " ";
+        }
+        cout << '\n';
+    }
+    cout << '\n';
 }
-/* 
-Set up CUDA on collab to run this code.
-First Change runtime to GPU and run these codes:
-!pip install git+https://github.com/andreinechaev/nvcc4jupyter.git
-%load_ext nvcc_plugin
-*/
+
+
+int main() {
+    int* A, * B, * C;
+
+    int N = 2;
+    int blockSize =  16;
+
+    int matrixSize = N * N;
+    size_t matrixBytes = matrixSize * sizeof(int);
+
+    A = new int[matrixSize];
+    B = new int[matrixSize];
+    C = new int[matrixSize];
+
+    initialize(A, N);
+    initialize(B, N);
+    cout << "Matrix A: \n";
+    print(A, N);
+
+    cout << "Matrix B: \n";
+    print(B, N);
+
+    
+    int* X, * Y, * Z;
+    // Allocate space
+    cudaMalloc(&X, matrixBytes);
+    cudaMalloc(&Y, matrixBytes);
+    cudaMalloc(&Z, matrixBytes);
+
+    // Copy values from A to X
+    cudaMemcpy(X, A, matrixBytes, cudaMemcpyHostToDevice);
+    
+    // Copy values from A to X and B to Y
+    cudaMemcpy(Y, B, matrixBytes, cudaMemcpyHostToDevice);
+
+    // Specify number of threads
+    dim3 threadsPerBlock(blockSize, blockSize);
+    // Set number of grids per block
+    dim3 blocksPerGrid((N + blockSize - 1) / blockSize, (N + blockSize - 1) / blockSize);
+
+    multiply<<<blocksPerGrid, threadsPerBlock>>>(X, Y, Z, N);
+
+    cudaMemcpy(C, Z, matrixBytes, cudaMemcpyDeviceToHost);
+    cout << "Multiplication of matrix A and B: \n";
+    print(C, N);
+
+    delete[] A;
+    delete[] B;
+    delete[] C;
+
+    cudaFree(X);
+    cudaFree(Y);
+    cudaFree(Z);
+
+    return 0;
+}
--- HPC/4_Vector_Addition.cpp
+++ HPC/4_Vector_Addition.cpp
@@ -1,96 +1,74 @@
 %%cu
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
- 
-// CUDA kernel. Each thread takes care of one element of c
-__global__ void vecAdd(double *a, double *b, double *c, int n)
-{
-    // Get our global thread ID
-    int id = blockIdx.x*blockDim.x+threadIdx.x;
- 
-    // Make sure we do not go out of bounds
-    if (id < n)
-        c[id] = a[id] + b[id];
-}
- 
-int main( int argc, char* argv[] )
-{
-    // Size of vectors
-    int n = 100000;
- 
-    // Host input vectors
-    double *h_a;
-    double *h_b;
-    //Host output vector
-    double *h_c;
- 
-    // Device input vectors
-    double *d_a;
-    double *d_b;
-    //Device output vector
-    double *d_c;
- 
-    // Size, in bytes, of each vector
-    size_t bytes = n*sizeof(double);
- 
-    // Allocate memory for each vector on host
-    h_a = (double*)malloc(bytes);
-    h_b = (double*)malloc(bytes);
-    h_c = (double*)malloc(bytes);
- 
-    // Allocate memory for each vector on GPU
-    cudaMalloc(&d_a, bytes);
-    cudaMalloc(&d_b, bytes);
-    cudaMalloc(&d_c, bytes);
- 
-    int i;
-    // Initialize vectors on host
-    for( i = 0; i < n; i++ ) {
-        h_a[i] = sin(i)*sin(i);
-        h_b[i] = cos(i)*cos(i);
+#include <iostream>
+using namespace std;
+
+__global__
+void add(int* A, int* B, int* C, int size) {
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (tid < size) {
+        C[tid] = A[tid] + B[tid];
     }
- 
-    // Copy host vectors to device
-    cudaMemcpy( d_a, h_a, bytes, cudaMemcpyHostToDevice);
-    cudaMemcpy( d_b, h_b, bytes, cudaMemcpyHostToDevice);
- 
-    int blockSize, gridSize;
- 
-    // Number of threads in each thread block
-    blockSize = 1024;
- 
-    // Number of thread blocks in grid
-    gridSize = (int)ceil((float)n/blockSize);
- 
-    // Execute the kernel
-    vecAdd<<<gridSize, blockSize>>>(d_a, d_b, d_c, n);
- 
-    // Copy array back to host
-    cudaMemcpy( h_c, d_c, bytes, cudaMemcpyDeviceToHost );
- 
-    // Sum up vector c and print result divided by n, this should equal 1 within error
-    double sum = 0;
-    for(i=0; i<n; i++)
-        sum += h_c[i];
-    printf("final result: %f\n", sum/n);
- 
-    // Release device memory
-    cudaFree(d_a);
-    cudaFree(d_b);
-    cudaFree(d_c);
- 
-    // Release host memory
-    free(h_a);
-    free(h_b);
-    free(h_c);
- 
-    return 0;
 }
 
-/* 
-Set up CUDA on collab to run this code.
-First Change runtime to GPU and run these codes:
-!pip install git+https://github.com/andreinechaev/nvcc4jupyter.git
-%load_ext nvcc_plugin
-*/
+
+void initialize(int* vector, int size) {
+    for (int i = 0; i < size; i++) {
+        vector[i] = rand() % 10;
+    }
+}
+
+void print(int* vector, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << vector[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int N = 4;
+    int* A, * B, * C;
+
+    int vectorSize = N;
+    size_t vectorBytes = vectorSize * sizeof(int);
+
+    A = new int[vectorSize];
+    B = new int[vectorSize];
+    C = new int[vectorSize];
+
+    initialize(A, vectorSize);
+    initialize(B, vectorSize);
+
+    cout << "Vector A: ";
+    print(A, N);
+    cout << "Vector B: ";
+    print(B, N);
+
+    int* X, * Y, * Z;
+    cudaMalloc(&X, vectorBytes);
+    cudaMalloc(&Y, vectorBytes);
+    cudaMalloc(&Z, vectorBytes);
+
+    cudaMemcpy(X, A, vectorBytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(Y, B, vectorBytes, cudaMemcpyHostToDevice);
+
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+
+    add<<<blocksPerGrid, threadsPerBlock>>>(X, Y, Z, N);
+
+    cudaMemcpy(C, Z, vectorBytes, cudaMemcpyDeviceToHost);
+
+    cout << "Addition: ";
+    print(C, N);
+
+    delete[] A;
+    delete[] B;
+    delete[] C;
+
+    cudaFree(X);
+    cudaFree(Y);
+    cudaFree(Z);
+
+    return 0;
+}
