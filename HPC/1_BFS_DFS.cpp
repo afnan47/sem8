eaa8be988fc65a5ca9d94008b148d68688a42c9c
Afnan Attar <65289967+afnan47@users.noreply.github.com>
2023-05-27 17:21:56 +0530

    Update 1_BFS_DFS.cpp

--- HPC/1_BFS_DFS.cpp
+++ HPC/1_BFS_DFS.cpp
@@ -5,11 +5,16 @@
 
 using namespace std;
 
-struct TreeNode{
+class TreeNode{
+    public:
     int val;
     TreeNode* left;
     TreeNode* right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    TreeNode(int x){
+        val = x;
+        left = NULL;
+        right = NULL;
+    }
 };
 
 void pBFS(TreeNode* root){
